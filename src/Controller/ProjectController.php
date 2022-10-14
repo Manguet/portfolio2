fe12095cdf5064276f69d393b2f01009f207388f
Benjamin <benjamin.manguet@negocian.fr>
2022-10-14 17:54:22 +0200

    projects starte

---
+++ src/Controller/ProjectController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @author Benjamin Manguet <manguetbenj@gmail.com>
+ */
+#[Route('/project', name: 'app_project_')]
+class ProjectController extends AbstractController
+{
+    #[Route('', 'index')]
+    public function index(): Response
+    {
+        return $this->render('project.html.twig');
+    }
+}
